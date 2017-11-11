8f17d95646c3995109d0cdbe5780cef1c8fa2537
oshrat <oshrat.y20@gmail.com>
2017-11-11 19:16:26 +0200

    recreated the project as a maven java project fix scrapers to get the correct text of the body of an article add google translate service cloud integration

---
+++ src/main/java/com/mycompany/netreali/txtTranslator.java
@@ -0,0 +1,28 @@
+package com.mycompany.netreali;
+
+// Imports the Google Cloud client library
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.Translate.TranslateOption;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
+public class txtTranslator {
+  public static void main(String... args) throws Exception {
+    // Instantiates a client
+    Translate translate = TranslateOptions.getDefaultInstance().getService();
+
+    // The text to translate
+    String text = "אזעקת השווא שגרמה לבהלה ביישובי גוש דן נבעה מטעות אנוש - כך עולה מתחקיר שנערך בראשות מפקד פיקוד העורף, אלוף תמיר ידעי. ב-2 בנובמבר התעוררו תושבים רבים באזור המרכז בשעה 2:48 בלילה לקול אזעקה. התושבים דיווחו על אזעקה עולה ויורדת שנשמעה בתל אביב, בגבעתיים, בת-ים, חולון ובאזורים נוספים בגוש דן. בתום התחקיר נמסר: \"פיקוד העורף הפיק לקחים מהאירוע, אותם יישם עד תום ופועל לצמצום מקרי אזעקות שווא במטרה לשמור על שגרת יומם של אזרחי ישראל\". האזעקה הופעלה על רקע מתיחות ביטחונית בשתי גזרות, מצפון ומדרום. שלושה ימים קודם לכן פוצצה המנהרה שחפר הג'יהאד האיסלאמי מרצועת עזה לתוך שטח ישראל. בפיצוץ נהרגו 12 מחבלים מהג'יאהד האיסלאמי ומהחמאס, בהם בכירים. התעורר חשש שלמרות התנגדות החמאס, בארגון הג'יהאד האיסלאמי יתקשו להבליג על המכה שספגו, וישגרו רקטות לעבר ישראל. מסיבה זו בוטל תרגיל של פיקוד העורף, שבמסגרתו היו אמורים לפעול צופרים ברחבי גוש דן. אזעקת השווא: על רקע חשש מתגובה של הג'יהאד על פיצוץ המנהרה(צילום: EPA) מצפון, הורגשה מתיחות לאחר שלפי דיווחים סוריים תקף חיל האוויר, שעות לפני שהופעלו הצופרים, מטרה באיזור העיר חומס. בתגובה, שיגרו הסורים טילי נ\"מ לעבר מטוסי חיל-האוויר. מצאתם טעות בכתבה? כתבו לנו";
+    
+    // Translates some text into Russian
+    Translation translation =
+        translate.translate(
+            text,
+            TranslateOption.sourceLanguage("iw"),
+            TranslateOption.targetLanguage("en"));
+
+
+    System.out.printf("Text: %s%n", text);
+    System.out.printf("Translation: %s%n", translation.getTranslatedText());
+  }
+}
