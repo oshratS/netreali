278b52c159656fd9f534531841dd7ffdf9b5f167
oshrat <oshrat.y20@gmail.com>
2017-11-28 19:59:51 +0200

    created result pages

---
+++ src/main/java/com/mycompany/netreali/CirclePanel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.netreali;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author oshrat
+ */
+public class CirclePanel extends JPanel {
+@Override
+protected void paintComponent(Graphics g) {
+    //Adding  super.paintComponent....  
+    super.paintComponent(g);
+
+    g.drawOval(0, 0, g.getClipBounds().width, g.getClipBounds().height);
+  }
+}
